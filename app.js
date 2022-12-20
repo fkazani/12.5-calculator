3efa1f353cea097bd3ca07c7ffe281dec61eec44
Faizan Kazani <user@users-Mac-mini.local>
2022-12-19 20:25:18 -0700

    Setup to create new elements for entries

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// Reading amount input value and assigning to amountInput variable
-var amountInput = document.getElementById("amount");
+// Global Variables 
 
-// Reading the button value and assining to element variable
-const element = document.getElementById("addBtn");
-
-// Assigning an initial 0 value to the outstanding total
-var outstandingAmount = 0;
+var dateEntered = document.getElementById("date"); // Reading the date value
+var amountInput = document.getElementById("amount"); // Reading amount input value and assigning to amountInput variable
+const element = document.getElementById("addBtn"); // Reading the button value and assining to element variable
+var outstandingAmount = 0; // Assigning an initial 0 value to the outstanding total
 document.getElementById("outstandingAmount").innerHTML = outstandingAmount;
-
-// Set the click handler to calcNewTotal function
-element.addEventListener("click", calcNewTotal);
+element.addEventListener("click", calcNewTotal); // Set the click handler to calcNewTotal function
 
 // Funtion to calculate and update the outstanding value amount
 function calcNewTotal () {
@@ -19,4 +15,18 @@ function calcNewTotal () {
     document.getElementById("outstandingAmount").innerHTML = newAmount;
 }
 
+// Function to create a new entry list item
+var createNewEntryListElement = function () {
+    var listEntry = document.createElement("li"); // create list entry
+    var date = document.createElement("p"); // entry date
+    var label = document.createElement("p"); // entry label
+    var amountEntry = document.createElement("p"); // entry amount 
+
+    // each element needs appending
+    listEntry.appendChild(date);
+    listEntry.appendChild(label);
+    listEntry.appendChild(amountEntry);
+
+    return listEntry;
+}
 
