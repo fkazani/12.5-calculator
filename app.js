ac581ed5f011013a443bec945bf94a0d49d2e3c5
Faizan Kazani <user@users-Mac-mini.local>
2022-12-22 18:57:31 -0700

    created a function for adding list entries to DOM

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Global Variables 
-
 var dateEntered = document.getElementById("date"); // Reading the date value
 var amountInput = document.getElementById("amount"); // Reading amount input value and assigning to amountInput variable
+var sourceInput = document.getElementById("source"); // Reading source value 
 const element = document.getElementById("addBtn"); // Reading the button value and assining to element variable
 var outstandingAmount = 0; // Assigning an initial 0 value to the outstanding total
 document.getElementById("outstandingAmount").innerHTML = outstandingAmount;
@@ -16,16 +16,26 @@ function calcNewTotal () {
 }
 
 // Function to create a new entry list item
-var createNewEntryListElement = function () {
-    var listEntry = document.createElement("li"); // create list entry
-    var date = document.createElement("p"); // entry date
-    var label = document.createElement("p"); // entry label
-    var amountEntry = document.createElement("p"); // entry amount 
+function createNewEntryListElement () {
+    const listEntry = document.createElement("li"); 
+    const date = document.createElement("p"); 
+    const dateNode = document.createTextNode(dateEntered.value);
+    date.appendChild(dateNode);
+    const source = document.createElement("p"); 
+    const sourceNode = document.createTextNode(sourceInput.value);
+    source.appendChild(sourceNode);
+    const amountEntry = document.createElement("p"); 
+    const amountNode = document.createTextNode(amountInput.value);
+    amountEntry.appendChild(amountNode);
 
     // each element needs appending
     listEntry.appendChild(date);
-    listEntry.appendChild(label);
+    listEntry.appendChild(source);
     listEntry.appendChild(amountEntry);
+
+    // appending list entries to "entris" element
+    entriesList = document.getElementById("entries");
+    entriesList.appendChild(listEntry);
 
     return listEntry;
 }
