cf5bbce642a06a78ec6ee80e5f55eae71f7ea8cc
Faizan Kazani <user@users-Mac-mini.local>
2023-01-12 07:54:12 -0700

    Added comments to server.js for better readability

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// Creating express server
 const express = require('express');
 const app = express();
 
+// Setting the app to use ejs view engine
 app.set('view engine', 'ejs');
 
+// App to use the render files under public folder
 app.use(express.static('public'));
 
+// Get request to render the application
 app.get("/", (req, res) => {
     res.render("index");
 })
 
+// Setting up port 3000 to listen the request
 app.listen(3000);
