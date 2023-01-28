998c22447634657feeaa4b857bf7c8e08438aa8a
Faizan Kazani <user@users-Mac-mini.local>
2023-01-27 20:56:50 -0700

    Installed sequelize, updated the server code to connect to express database through sequelize

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 // Creating express server
 import express from 'express';
 import mysql from 'mysql2';
+import { Sequelize } from 'sequelize';
 const app = express();
 
-// Configuration
-const pool = mysql.createConnection({
-  host     : '127.0.0.1',
-  user     : 'root',
-  password : 'E00md3sign',
-  database : 'express'
+const sequelize = new Sequelize ('express', 'root', 'E00m$arch', {
+  host: 'localhost',
+  dialect: 'mysql',
+  dialectModule: mysql
 });
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+// Configuration
+// const pool = mysql.createConnection({
+//   host     : '127.0.0.1',
+//   user     : 'root',
+//   password : 'E00md3sign',
+//   database : 'express'
+// });
 
 // App to use the render files under public folder
 app.use(express.static('public'));
@@ -19,13 +33,13 @@ app.set('view engine', 'ejs');
  
 // Get request to render the application
 app.get("/", (req, res) => {
-    pool.connect();
+    // pool.connect();
 
-    pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-        if (error) throw error;
+    // pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    //     if (error) throw error;
         
-        console.log('The solution is: ', results[0].solution);
-    });
+    //     console.log('The solution is: ', results[0].solution);
+    // });
  
     res.render("index");
 })
